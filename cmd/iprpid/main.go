2640974b6f8d6d988aae3badfb937378f9824fab
Adrian Lanzafame <adrianlanzafame92@gmail.com>
2019-06-28 19:02:22 +0200

    get id from api

--- cmd/iprpid/main.go
+++ cmd/iprpid/main.go
@@ -2,34 +2,17 @@ package main
 
 import (
 	"context"
-	"flag"
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/ipfs/ipfs-cluster/api/rest/client"
 	"github.com/lanzafame/bobblehat/sense/screen"
 	iprpic "github.com/lanzafame/ipfs-cluster-iprpic"
-	peer "github.com/libp2p/go-libp2p-peer"
 )
 
 func main() {
-	var p = flag.String("p", "", "peer id of current cluster peer")
-	flag.Parse()
-
-	if *p == "" {
-		fmt.Println("please provide peer id of the cluster peer running on this machine")
-		os.Exit(-1)
-	}
-
 	log.Print("loading...")
 	ctx, cancel := context.WithCancel(context.Background())
 	go iprpic.SignalHandler(ctx, cancel)
-
-	pid, err := peer.IDB58Decode(*p)
-	if err != nil {
-		panic(err)
-	}
 
 	cfg := &client.Config{}
 	c, err := client.NewDefaultClient(cfg)
@@ -37,8 +20,13 @@ func main() {
 		panic(err)
 	}
 
+	p, err := c.ID(ctx)
+	if err != nil {
+		panic(err)
+	}
+
 	fb := screen.NewFrameBuffer()
-	s := iprpic.NewStatuses(fb, pid, c)
+	s := iprpic.NewStatuses(fb, p.ID, c)
 	ctr := iprpic.NewCounter(fb, c, iprpic.DefaultSegments)
 
 	go s.DrawPeerStatuses(ctx)
